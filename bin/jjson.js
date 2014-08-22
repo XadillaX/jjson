2147ec04473ec32cc8c9847aa0fafadd13f1c7ba
XadillaX <admin@xcoder.in>
2014-08-22 14:27:05 +0800

    vim plugin mode added

--- bin/jjson.js
+++ bin/jjson.js
@@ -21,6 +21,10 @@ var opts = require("nomnom").option("file", {
     abbr    : "i",
     help    : "Number of indent for each line.",
     default : 2
+}).option("vim-plugin-mode", {
+    abbr    : "v",
+    help    : "Whether it's in VIM plugin mode.",
+    flag    : true
 }).script("jjson").parse();
 
 var filename = opts.file;
@@ -28,10 +32,12 @@ fs.readFile(filename, { encoding: opts.encoding }, function(err, json) {
     if(err) {
         return console.error("Error occurred while reading file: " + err.message);
     }
-    
+
+    var orig = json;
     try {
         json = JSON.stringify(JSON.parse(json), true, opts.indent);
     } catch(e) {
+        if(opts["vim-plugin-mode"]) return console.log(orig);
         return console.error("Error occurred while parsing file: " + e.message);
     }
 
